f0234b01f75dab363ca062b6acf81b54421a5927
AbdelqaderAfnan <abdelqader@afnangroup.com>
2020-12-05 14:25:33 +0200

    add all models and maigrations

---
+++ app/Http/Controllers/DocDateController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Doc_date;
+use Illuminate\Http\Request;
+
+class DocDateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Doc_date  $doc_date
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Doc_date $doc_date)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Doc_date  $doc_date
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Doc_date $doc_date)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Doc_date  $doc_date
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Doc_date $doc_date)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Doc_date  $doc_date
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Doc_date $doc_date)
+    {
+        //
+    }
+}
---
+++ database/factories/DocDateFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Doc_date;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DocDateFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Doc_date::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
